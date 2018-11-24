be97517a3f18b8ed83e0d5cf4936e1ad1e124921
Joe Martin <josephmartin39d@gmail.com>
2018-11-24 16:40:48 +0000

    Initial Commit

---
+++ Meditation Color Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meditation Color Timer
+//
+//  Created by Joe Martin on 24/11/2018.
+//  Copyright © 2018 Joe Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
